b99b893928fecb561e78b3bf13146d08232c28d8
Adrian Ortega <hello@aortegadesign.com>
2023-08-01 23:02:07 -0700

    commands wip

--- src/stores/commands.js
+++ src/stores/commands.js
@@ -1,7 +1,21 @@
 import { defineStore } from "pinia";
+import { computed, reactive } from "vue";
 
 export const useCommandsStore = defineStore("commands", () => {
-  return {
+  const state = reactive({
+    items: [],
+    pagination: {
+      page: 1,
+      per_page: 10
+    }
+  });
 
+  const fetchItems = () => {
+
+  }
+
+  return {
+    items: computed(() => state.items),
+    pagination: computed(() => state.pagination)
   }
 });
