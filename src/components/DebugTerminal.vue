edf6521fb6d0b35fc05c4763c5913c248844d7cc
Adrian Ortega <adrian@aortegadesign.com>
2023-08-30 08:56:41 -0700

    Adds debug terminal

---
+++ src/components/DebugTerminal.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { computed, reactive } from 'vue'
+import { useWebsocketStore } from '../stores/websocket'
+const ws = useWebsocketStore()
+const state = reactive({ items: [] })
+
+ws.onMessage(({ event, payload }) => {
+  if (event !== 'log') return
+  state.items.push({ ...payload })
+})
+
+const items = computed(() => state.items)
+</script>
+
+<template>
+  <div class="overlay-terminal">
+    <div
+      v-for="item in items"
+      class="overlay-terminal__line"
+      :class="{
+        'is-error': [item.levels.error, item.levels.fatal].includes(item.type),
+        'is-warning': item.levels.warn === item.type,
+        'is-success': item.levels.success === item.type,
+        'is-info': [item.levels.info, item.levels.debug].includes(item.type)
+      }"
+    >
+      <span class="ts">{{ item.timestamp }}</span>
+      <span class="pre">[{{ item.prefix }}]</span>
+      <span class="msg">{{ item.message }}</span>
+      <span v-if="item.context && item.context.message" class="ctx">{{
+        item.context.message
+      }}</span>
+    </div>
+  </div>
+</template>
--- src/views/DebugView.vue
+++ src/views/DebugView.vue
@@ -2,13 +2,17 @@
 import Chat from '../components/Chat.vue'
 import BrowserSourceOverlay from '../components/BrowserSourceOverlay.vue'
 import PageHeader from '../components/PageHeader.vue'
+import DebugTerminal from '../components/DebugTerminal.vue'
 </script>
 
 <template>
   <PageHeader title="Debug" />
   <div class="container">
     <div class="content">
-      <BrowserSourceOverlay />
+      <div class="overlay-debug">
+        <BrowserSourceOverlay />
+      </div>
+      <DebugTerminal />
     </div>
     <div class="sidebar">
       <Chat />
