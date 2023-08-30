ae52a716c7b19c0074a8d0f23d7c2b5e4451815e
Adrian Ortega <adrian@aortegadesign.com>
2023-08-30 10:20:37 -0700

    updates debug terminal styling and structure

--- src/components/DebugTerminal.vue
+++ src/components/DebugTerminal.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { computed, reactive } from 'vue'
+import { computed, reactive, ref, watch } from 'vue'
 import { useWebsocketStore } from '../stores/websocket'
 const ws = useWebsocketStore()
+const lines = ref(null)
 const state = reactive({ items: [] })
 
 ws.onMessage(({ event, payload }) => {
@@ -18,26 +19,34 @@ const items = computed(() =>
     )
   }))
 )
+
+watch(state.items, () => {
+  setTimeout(() => {
+    lines.value.scrollTop = lines.value.scrollHeight
+  }, 1)
+})
 </script>
 
 <template>
   <div class="overlay-terminal">
-    <div
-      v-for="item in items"
-      class="overlay-terminal__line"
-      :class="{
-        'is-error': [item.levels.error, item.levels.fatal].includes(item.type),
-        'is-warning': item.levels.warn === item.type,
-        'is-success': item.levels.success === item.type,
-        'is-info': [item.levels.info, item.levels.debug].includes(item.type)
-      }"
-    >
-      <span class="ts">{{ item.timestamp }}</span>
-      <span class="pre">[{{ item.prefix }}]</span>
-      <span class="msg">{{ item.message }}</span>
-      <span v-if="item.context && item.context.message" class="ctx">{{
-        item.context.message
-      }}</span>
+    <div class="overlay-terminal__container" ref="lines">
+      <div
+        v-for="item in items"
+        class="overlay-terminal__line"
+        :class="{
+          'is-error': [item.levels.error, item.levels.fatal].includes(item.type),
+          'is-warning': item.levels.warn === item.type,
+          'is-success': item.levels.success === item.type,
+          'is-info': [item.levels.info, item.levels.debug].includes(item.type)
+        }"
+      >
+        <span class="ts">{{ item.timestamp }}</span>
+        <span class="pre">[{{ item.prefix }}]</span>
+        <span class="msg">{{ item.message }}</span>
+        <span v-if="item.context && item.context.message" class="ctx">{{
+          item.context.message
+        }}</span>
+      </div>
     </div>
   </div>
 </template>
