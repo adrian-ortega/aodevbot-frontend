abf0b26e6432fd311f52120991c8d5331660fce2
Adrian Ortega <adrian@aortegadesign.com>
2023-08-31 16:41:15 -0700

    adds sp player

--- src/components/BrowserSourceOverlay.vue
+++ src/components/BrowserSourceOverlay.vue
@@ -1,12 +1,13 @@
 <script setup>
 import ChannelRedemptionFirst from './ChannelRedemptionFirst.vue'
 import TimeCommand from './overlay/TimeCommand.vue'
-import HugCommand
- from './overlay/HugCommand.vue'
+import HugCommand from './overlay/HugCommand.vue'
+import SpotifyPlayer from './overlay/SpotifyPlayer.vue'
 </script>
 
 <template>
   <TimeCommand />
-  <HugCommand/>
+  <HugCommand />
+  <SpotifyPlayer />
   <ChannelRedemptionFirst />
 </template>
---
+++ src/components/overlay/SpotifyPlayer.vue
@@ -0,0 +1,29 @@
+<script setup>
+import placeholderImg from '../../assets/gifs/friends-hug.gif'
+</script>
+<template>
+  <div class="spotify-player animate-in">
+    <div class="spotify-player__container">
+      <div class="spotify-player__track">
+        <div class="spotify-player__album">
+          <img :src="placeholderImg" alt="" />
+        </div>
+        <div class="spotify-player__content">
+          <div class="spotify-player__title">
+            <div :class="{ marquee: false }">
+              <div>A Song Title</div>
+            </div>
+          </div>
+          <div class="spotify-player__artist">
+            <div :class="{ marquee: false }">
+              <div>Artist</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="spotify-player__progress">
+        <div :style="{ width: '25%' }"></div>
+      </div>
+    </div>
+  </div>
+</template>
